e54601e421fecc35b9912debb2a405bdb20b844d
DELL <DELL@DESKTOP-EBJGPVB>
2021-11-21 23:28:59 +0300

    end of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         public StoreContext _context { get; }
         public ProductsController(StoreContext context)
@@ -26,9 +26,13 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")] // api/products/3
-        public async Task<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            return product;
         }
     }
 }
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,12 +6,17 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { Route } from "react-router";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+import { Switch } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -31,14 +36,20 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
+        <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/catalog' component={Catalog} />
         <Route path='/catalog/:id' component={ProductDetails} />
         <Route path='/about' component={AboutPage} />
         <Route path='/contact' component={ContactPage} />
+        <Route path='/server-error' component={ServerError} />
+        <Route component={NotFound} />
+        </Switch>
+       
       </Container>
     </ThemeProvider>
   );

b508c06976003a23ad0951dce93f7e09b6f311ca
DELL <DELL@DESKTOP-EBJGPVB>
2021-12-07 23:50:55 +0300

    ara komit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,10 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { StoreProvider } from './app/context/StoreContext';
-import { configureStore } from './app/store/configureStore';
 import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
 
-const store = configureStore();
 export const history = createBrowserHistory();
 
 ReactDOM.render(

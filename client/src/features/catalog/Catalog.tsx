7d3528b35d053c67ae01266f133352e759978516
DELL <DELL@DESKTOP-EBJGPVB>
2021-12-16 00:16:12 +0300

    ara commit

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,22 @@
 import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
       if(!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch]); //dependency, boş array 1 kereç çalışsın demek,  hiç koymazsan sonsuz çalışır
-  
+
+    useEffect(() => {
+        if(!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded]); 
+
     if(status.includes('pending')) return <LoadingComponent message='Loading products...' />
 
     return (
